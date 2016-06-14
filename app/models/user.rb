0f4f674d9e9b224a6312f8ea2d4ec90d5215040f
yirui <yirui94@gmail.com>
2016-06-14 18:41:15 +0800

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,18 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token
+	attr_accessor :remember_token, :activation_token
 
 	validates :name, presence: true, length: { maximum: 50}
 
-	before_save { self.email = self.email.downcase }
+	before_save :downcase_email
+	before_create :create_activation_digest
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\d.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255},
 										format: { with: VALID_EMAIL_REGEX},
 										uniqueness: { case_sensitive: false } 
 	has_secure_password
 	validates :password, length: { minimum: 8 }, allow_blank: true
+
+
 
 	def User.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -26,12 +29,25 @@ class User < ActiveRecord::Base
 		update_attribute(:remember_digest, User.digest(remember_token))
 	end
 
-	def authenticated?(remember_token)
-		return false if remember_digest.nil?
-		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	def authenticated?(attribute, token)
+		digest = send("#{attribute}_digest")
+		return false if digest.nil?
+		BCrypt::Password.new(digest).is_password?(token)
 	end
 
 	def forget
 		update_attribute(:remember_digest, nil)
 	end
+
+	private
+
+		def downcase_email
+			self.email = self.email.downcase
+		end
+
+
+		def create_activation_digest
+			self.activation_token = User.new_token
+			self.activation_digest = User.digest(activation_token)
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   get 'signup' => 'users#new'
   resources :users
-
+  resources :account_activations, only: [:edit]
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
